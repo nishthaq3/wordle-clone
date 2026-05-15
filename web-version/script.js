37241115eff88cd61fa734965eac1c803fbcb5f1
nishthaq3 <nishthatripathi05@gmail.com>
2026-05-15 15:06:50 +0530

    Added keyboard input handling with backspace navigation and enter key validation

--- web-version/script.js
+++ web-version/script.js
@@ -1,9 +1,47 @@
 const board = document.querySelector(".board");
 
-for(let i = 0; i < 30; i++) {
-    const tile = document.createElement("div");
+const rows=6;
+const cols=5;
 
-    tile.classList.add("tile");
 
-    board.appendChild(tile);
+let currentRow=0;
+let currentCol=0;
+
+const tiles=[];
+
+for(let r=0;r<rows;r++){
+   const row=[]
+    for(let c=0;c<cols;c++){
+       const tile=document.createElement("div");
+       tile.classList.add("tile");
+       board.appendChild(tile);
+       row.push(tile);
+    }
+    tiles.push(row);
 }
+
+document.addEventListener("keydown", (event) => {
+
+    const key = event.key;
+
+    if(currentCol < cols && /^[a-zA-Z]$/.test(key)) {
+
+        tiles[currentRow][currentCol].textContent = key.toUpperCase();
+
+        currentCol++;
+    }
+    //adding backspace feature
+    else if(key==="Backspace"){
+        if(currentCol>0){
+            currentCol--;
+            tiles[currentRow][currentCol].textContent="";
+        }
+    }
+    //move to next line if pressed enter
+    else if(key==="Enter"){
+        if(currentCol===cols){
+            currentRow++;
+            currentCol=0;
+        }
+    }
+});
